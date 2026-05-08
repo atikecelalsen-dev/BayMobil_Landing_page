dba660a23c37cb0ac5e1e82860dafbc57deb4157
Atike <hatikecelalsen@gmail.com>
2026-05-08 14:28:24 +0300

    ..

---
+++ BayMobil_Landing_page/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BayMobil_Landing_page.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
