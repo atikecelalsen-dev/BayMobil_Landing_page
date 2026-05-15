d977cb30d05b9cc4274aedf6af99f98850278227
Atike <hatikecelalsen@gmail.com>
2026-05-15 10:55:35 +0300

    ..

---
+++ BayMobil_Landing_page/Controllers/WhatsAppTestController.cs
@@ -0,0 +1,74 @@
+﻿//using BayMobil_Landing_page.Helpers;
+using BayMobil_Landing_page.Models;
+using WhatsAppCloudApi;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BayMobil_Landing_page.Controllers
+{
+    public class WhatsAppTestController : Controller
+    {
+        //private readonly WhatsAppHelper _whatsAppHelper;
+
+        //public WhatsAppTestController(WhatsAppHelper whatsAppHelper)
+        //{
+        //    _whatsAppHelper = whatsAppHelper;
+        //}
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new WhatsAppTestModel());
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> Index(WhatsAppTestModel model)
+        //{
+        //    try
+        //    {
+        //        var sonuc = await _whatsAppHelper.Send(
+        //            model.GsmNo,
+        //            model.Mesaj,
+        //            model.GonderilecekDosyaUrl
+        //        );
+
+        //        model.Success = sonuc.Success;
+        //        model.StatusCode = sonuc.StatusCode;
+        //        model.ApiResponse = sonuc.ResponseText;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        model.Success = false;
+        //        model.ApiResponse = ex.ToString();
+        //    }
+
+        //    return View(model);
+        //}
+
+
+
+        private readonly IConfiguration _configuration;
+
+        public WhatsAppTestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(WhatsAppTestModel model)
+        {
+            WhatsAppHelper.AccessToken = _configuration["WhatsApp:AccessToken"];
+            WhatsAppHelper.PhoneNumberId = _configuration["WhatsApp:PhoneNumberId"];
+
+            WhatsAppResult sonuc = await WhatsAppHelper.Send(
+                model.GsmNo,
+                model.Mesaj,
+                model.GonderilecekDosyaUrl
+            );
+
+            model.Success = sonuc.Success;
+            model.StatusCode = sonuc.StatusCode;
+            model.ApiResponse = sonuc.ResponseText ?? sonuc.ErrorMessage;
+
+            return View(model);
+        }
+    }
+}
---
+++ BayMobil_Landing_page/Models/WhatsAppTestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BayMobil_Landing_page.Models
+{
+    public class WhatsAppTestModel
+    {
+        public string GsmNo { get; set; }
+        public string Mesaj { get; set; }
+        public string GonderilecekDosyaUrl { get; set; }
+        public string StatusCode { get; set; }
+        public bool? Success { get; set; }
+        public string ApiResponse { get; set; }
+    }
+}
